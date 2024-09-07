1837ba1a94d4cf3fb4fb9db53a77d9a5f4721353
khaled al-amoudi <xxxkhald@gmail.com>
2024-09-07 18:21:53 +0300

    second commit

--- lib/core/const/const_color.dart
+++ lib/core/const/const_color.dart
@@ -1,6 +1,6 @@
  import 'package:flutter/material.dart';
 
 class ConstColor {
-  static Color primaryColor=Color(0xffff7b2e);
-  static Color productTitleColor=Color(0xff68bec0);
+  static Color primaryColor=const Color(0xffff7b2e);
+  static Color productTitleColor=const Color(0xff68bec0);
 }
